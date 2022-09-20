a70d2b6d2ef0c55ba76ae6aa89264c1c50fcab07
Michael Bowlin <jaidoutani@gmail.com>
2022-09-20 16:18:52 -0500

    Completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Links() {
+  return (
+    <div>
+        <h3>Links</h3>
+        <a href='https://github.com/liza'> https://github.com/liza </a> 
+        <a href='https://www.linkedin.com/in/liza/'>https://www.linkedin.com/in/liza/</a>
+    </div>
+  )
+}
